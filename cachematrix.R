ed2514ee0f6969808315c490586b367c422d7377
Shubham Jha <shubhamjha28@gmail.com>
2016-11-14 05:12:52 +0530

    Added the overall description of the file

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Here the given two functions are used for taking a matrix as an
+## argument and stores it in a special object that is created by these
+## functions and caches its inverse.
+
+
 ## Write a short comment describing this function
 
+## Here the function "makeCacheMatrix" is used to crate a special matrix
+## object that can store its inverse for later use.
+
 makeCacheMatrix <- function(x = matrix()) {
+
 
 }
 
