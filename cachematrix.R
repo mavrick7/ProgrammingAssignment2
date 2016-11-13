237879e17d5d498e74963fa3937d333a2b340d54
Shubham Jha <shubhamjha28@gmail.com>
2016-11-14 05:22:26 +0530

    commiting the function makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -12,6 +12,23 @@
 ## object that can store its inverse for later use.
 
 makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+		
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+		
+        get <- function() x
+        
+		setInverse <- function(inverse) i <<- inverse
+        
+		getInverse <- function() i
+        
+		list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 
 
 }
